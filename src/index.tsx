643ea6209226235b1deabc3ce07dbe6c81cee42e
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-09 19:47:29 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <CssVarsProvider>
-        <demo />
+//        <demo />
 
       </CssVarsProvider>
     </StyledEngineProvider>
