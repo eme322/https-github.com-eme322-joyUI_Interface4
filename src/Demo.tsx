099e52d9273b720e9f8a61510f7cd7d84746aad5
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-15 18:56:35 -0400

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,21 +1,9 @@
 import * as React from 'react';
 import SignIn from './SignIn'; 
-// import Button from '@mui/joy/Button';
-// import Box from '@mui/joy/Box';
-
 
 export default function Demo() {
   return (
-    
-//    <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
-//      <Button>Button 000000 </Button>
-//      <Button disabled>Disabled</Button>
-//      <Button loading>Loading</Button>
-//      <Button >Click me</Button>
-//      <Button> Hola Hey!!!!!!!!!!!</Button>
-    <Button>Jajaja </Button>
-      
-//    </Box>
+     <SignIn />
   );
 }
 
