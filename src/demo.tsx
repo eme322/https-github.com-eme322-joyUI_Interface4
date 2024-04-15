cc64fbcd47e330f32165bd0e72325c3d3db6abb6
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-15 14:15:55 -0400

    Update demo.tsx

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,66 +1,16 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
+import * as React from 'react';
 import Button from '@mui/joy/Button';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import Box from '@mui/joy/Box';
 
-const Demo: React.FC = () => {
+export default function BasicButtons() {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        minHeight: '100vh',
-        backgroundColor: '#fff',
-        padding: 4
-      }}
-    >
-      <img src="https://www.boardx.us/content/images/2023/11/BoardX-Logo.png" alt="Logo" width="80" height="80" />
-      <Typography variant="h4" component="h1" sx={{ margin: 2 }}>
-        Welcome to Boardx!
-      </Typography>
-      <Box component="form" sx={{ width: '100%', maxWidth: '360px' }}>
-        <TextField
-          fullWidth
-          label="Email"
-          margin="normal"
-          variant="outlined"
-        />
-        <TextField
-          fullWidth
-          label="Password"
-          margin="normal"
-          variant="outlined"
-          type="password"
-        />
-        <Button
-          sx={{
-            marginTop: 2,
-            backgroundColor: '#E91E63',
-            ':hover': {
-              backgroundColor: '#AD1457',
-            },
-            color: '#fff',
-            paddingY: '10px',
-            borderRadius: '20px',
-          }}
-          fullWidth
-          variant="contained"
-        >
-          Sign In
-        </Button>
-        <Typography
-          sx={{ cursor: 'pointer', textAlign: 'center', marginY: 2 }}
-          color="text.secondary"
-        >
-          Forgot Password
-        </Typography>
-      </Box>
+    <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
+      <Button>Button 000000 </Button>
+      <Button disabled>Disabled</Button>
+      <Button loading>Loading</Button>
+      <Button >Click me</Button>
+      
     </Box>
   );
-};
-
-export default Demo;
+}
 
