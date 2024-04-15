6d1f1aaf76b4370bc27ddbb67b77849c360da022
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-15 18:33:25 -0400

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,26 +1,20 @@
 import * as React from 'react';
-import Button from '@mui/joy/Button';
-import Box from '@mui/joy/Box';
+import SignInForm from './SignIn'; 
+// import Button from '@mui/joy/Button';
+// import Box from '@mui/joy/Box';
 
 
-
-import Textarea from '@mui/joy/Textarea';
-
-export default function MyApp() {
-  return <Textarea placeholder="Type anything…" />;
-}
-
-
-export default function BasicButtons() {
+export default function Demo() {
   return (
-    <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
-      <Button>Button 000000 </Button>
-      <Button disabled>Disabled</Button>
-      <Button loading>Loading</Button>
-      <Button >Click me</Button>
-      <Button> Hola Hey!!!!!!!!!!!</Button>
+    
+//    <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
+//      <Button>Button 000000 </Button>
+//      <Button disabled>Disabled</Button>
+//      <Button loading>Loading</Button>
+//      <Button >Click me</Button>
+//      <Button> Hola Hey!!!!!!!!!!!</Button>
       
-    </Box>
+//    </Box>
   );
 }
 
