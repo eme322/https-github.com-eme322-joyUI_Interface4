35fda628bc5a8b6f23ca143d58d8a90076a7af96
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-15 20:18:10 -0400

    Update Demo.tsx

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SignIn from './subdirectory/SignIn'; // Adjust the import path based on your folder structure
+import SignIn from './SignIn'; // Adjust the import path based on your folder structure
 
 const Demo: React.FC = () => {
   return <SignIn />;
