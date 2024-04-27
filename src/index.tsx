4c6fadc762adbded6b991b55f99aafab2f7685f2
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-27 18:46:27 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
 import Demo from './Demo';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error('Failed to find the root element');
-}
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
+ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <CssVarsProvider>
