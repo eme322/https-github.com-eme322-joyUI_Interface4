10c764614e3fbe1e7076617736ed352f6bce0c17
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-28 13:00:04 -0400

    Create styles.ts

---
+++ src/components/styles.ts
@@ -0,0 +1,28 @@
+// joyUI_Interface1/src/components/styles.ts
+
+import styled from '@emotion/styled';
+import { Global, css } from '@emotion/react';
+
+export const ForgotPasswordLink = styled.a`
+  width: 450px;
+  color: #D8819E;
+  display: inline-block;
+  padding: 12px;
+  border: 1px solid #D8819E;
+  border-radius: 4px;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+  &:hover {
+    background-color: #430A1F;
+  }
+`;
+export const globalStyles = <Global
+  styles={css`
+    body {
+      margin: 0;
+      padding: 0;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    }
+  `}
+/>
