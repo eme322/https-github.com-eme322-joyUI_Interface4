f8efa8e80e126538d4d85108ebb234f19eb30e26
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-27 23:08:54 -0400

    Update ForgotPassword.tsx

--- src/components/ForgotPassword.tsx
+++ src/components/ForgotPassword.tsx
@@ -2,10 +2,8 @@ import * as React from 'react';
 
 export default function BasicButtons() {
   return (
-    <a href="/forgot-password" style={{ textDecoration: 'none' }}>
-      <span style={{ width: 450, color: '#D8819E', display: 'inline-block', padding: '12px', border: '1px solid #D8819E', borderRadius: '4px', textAlign: 'center', cursor: 'pointer', '&:hover': { backgroundColor: '#430A1F' } }}>
-        Forgot Password
-      </span>
+    <a href="/forgot-password" style={{ width: 450, color: '#D8819E', display: 'inline-block', padding: '12px', border: '1px solid #D8819E', borderRadius: '4px', textAlign: 'center', textDecoration: 'none', cursor: 'pointer', '&:hover': { backgroundColor: '#430A1F' } }}>
+      Forgot Password
     </a>
   );
 }
