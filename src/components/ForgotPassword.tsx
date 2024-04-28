af588c67dabf77b909036aac222532779b76f3ce
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-28 12:41:18 -0400

    Update ForgotPassword.tsx

--- src/components/ForgotPassword.tsx
+++ src/components/ForgotPassword.tsx
@@ -1,20 +1,5 @@
 import * as React from 'react';
-import styled from '@emotion/styled';
-
-const ForgotPasswordLink = styled.a`
-  width: 450px;
-  color: #D8819E;
-  display: inline-block;
-  padding: 12px;
-  border: 1px solid #D8819E;
-  border-radius: 4px;
-  text-align: center;
-  text-decoration: none;
-  cursor: pointer;
-  &:hover {
-    background-color: #430A1F;
-  }
-`;
+import { ForgotPasswordLink } from './styles';
 
 export default function BasicButtons() {
   return (
