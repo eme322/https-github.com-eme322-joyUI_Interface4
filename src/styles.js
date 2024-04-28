812b46c25115d64200ba49a99087a284d2e1ccd4
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-28 12:39:41 -0400

    Create styles.js

---
+++ src/styles.js
@@ -0,0 +1,17 @@
+// BasicButtons.styles.js
+import styled from '@emotion/styled';
+
+export const ForgotPasswordLink = styled.a`
+  width: 450px;
+  color: #D8819E;
+  display: inline-block;
+  padding: 12px;
+  border: 1px solid #D8819E;
+  border-radius: 4px;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+  &:hover {
+    background-color: #430A1F;
+  }
+`;
