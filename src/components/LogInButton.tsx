1c551790540ee39c7e5d20f593d7b3f0f167aecc
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-27 21:47:36 -0400

    Update LogInButton.tsx

--- src/components/LogInButton.tsx
+++ src/components/LogInButton.tsx
@@ -4,10 +4,11 @@ import Button from '@mui/joy/Button';
 export default function BasicButtons() {
   const handleClick = () => {
     console.log("Button clicked!");
-    // Add your desired functionality here
+    
+    
   };
 
   return (
-    <Button onClick={handleClick} sx={{ width: 450, backgroundColor: '#F21D6B', '&:hover': { backgroundColor: '#FF4081' } }}>Log In</Button>
+    <Button onClick={handleClick} sx={{ width: 450, backgroundColor: '#F21D6B', '&:hover': { backgroundColor: '#FF4081' } }}>Sign In</Button>
   );
 }
