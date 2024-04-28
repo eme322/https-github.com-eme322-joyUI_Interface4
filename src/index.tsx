4ffa32be186d98ea0e66dd74dda43f03df606b71
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-04-28 12:44:18 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
 import Demo from './Demo';
-import styles from './styles';
+import { globalStyles } from './styles';
+
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
